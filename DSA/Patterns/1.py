5b655a5bd8cbae69d6be5d2bbb0fd81000380411
ADAM M SANADI <adamsanadi6@gmail.com>
2024-10-15 23:48:53 +0530

    Solved Some pattern related problems

--- DSA/Patterns/1.py
+++ DSA/Patterns/1.py
@@ -1,11 +1,11 @@
 """
-Problem statement
-Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with *.
+Problem statement:
+Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
 
-For every value of N, help sam to print the corresponding N-dimensional forest.
+For every value of ‘N’, help sam to print the corresponding N-dimensional forest.
 
 Example:
-Input: N = 3
+Input: ‘N’ = 3
 
 Output: 
 * * *
